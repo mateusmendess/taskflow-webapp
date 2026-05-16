45fdfef891845707f48ccbdebe3ed13e73c3055e
Mateus <mateusxmendes@gmail.com>
2026-05-16 20:53:22 -0300

    fix: inicializar mail no create_app

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 # Inicialização da aplicação Flask
 from flask import Flask
 from config import Config
-from .extensions import db, bcrypt, login_manager
+from .extensions import db, bcrypt, login_manager, mail
 
 def create_app():
     app = Flask(__name__)
@@ -11,6 +11,7 @@ def create_app():
     db.init_app(app)
     bcrypt.init_app(app)
     login_manager.init_app(app)
+    mail.init_app(app)
 
     from .routes import main
     app.register_blueprint(main)
