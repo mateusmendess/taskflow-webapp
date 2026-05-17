1d40a0f4766ebc8ebc260a423a8a781a0ac60308
Mateus <mateusxmendes@gmail.com>
2026-05-16 22:36:57 -0300

    feat: adicionar configurações de email no config.py

--- config.py
+++ config.py
@@ -15,3 +15,11 @@ class Config:
     SQLALCHEMY_DATABASE_URI = uri
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Flask-Mail
+    MAIL_SERVER   = "smtp.gmail.com"
+    MAIL_PORT     = 587
+    MAIL_USE_TLS  = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    MAIL_DEFAULT_SENDER = ("TaskFlow", os.environ.get("MAIL_USERNAME"))
