9017186412de5d1be1702303e9604678e78f1f0c
Mateus <mateusxmendes@gmail.com>
2026-05-16 22:33:32 -0300

    feat: adicionar Mail ao extensions.py

--- app/extensions.py
+++ app/extensions.py
@@ -1,13 +1,13 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
-db = SQLAlchemy()
-
-bcrypt = Bcrypt()
-
+db            = SQLAlchemy()
+bcrypt        = Bcrypt()
 login_manager = LoginManager()
+mail          = Mail()
 
-login_manager.login_view = "main.login"
-login_manager.login_message = "Faça login para acessar esta página."
+login_manager.login_view             = "main.login"
+login_manager.login_message          = "Faça login para acessar esta página."
 login_manager.login_message_category = "info"
